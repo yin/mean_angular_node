8931faf5ec560c989ccb22e9037227c0326457de
jpiovar <jpiovar@sk.ibm.com>
2017-02-05 01:33:04 +0100

    no message

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     stylus = require('stylus'),
     logger = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -22,6 +23,17 @@ app.use(stylus.middleware(
   }
 ));
 app.use(express.static(__dirname + '/public'));
+
+mongoose.connect('mongodb://localhost/mean');
+var db = mongoose.connection;
+db.on('error',console.error.bind(console,' error mongoose ...'));
+db.once('open', function callback(){
+  console.log(' ok mongoose ... db opened');
+});
+
+app.get('/partials/:partialPath', function(req,res){
+  res.render('partials/' + req.params.partialPath);
+});
 
 app.get('*', function(req, res){
   res.render('index');
